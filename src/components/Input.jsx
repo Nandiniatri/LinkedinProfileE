7006e9f46e30925e8a629638aa247552f2f2e9dd
Nandiniatri <atrinandini8@gmail.com>
2025-10-02 23:29:21 +0530

    make a input compoents

---
+++ src/components/Input.jsx
@@ -0,0 +1,5 @@
+const Input = ({ type, placeholder, onClick, className }) => (
+    <input type={type} placeholder={placeholder} onClick={onClick} className={className} />
+)
+
+export default Input;

8a98ea0d87e0ac9fc52fcbf33414ea5ed28813af
Nandiniatri <atrinandini8@gmail.com>
2025-10-03 11:24:08 +0530

    make a home page better

--- src/linkedinProfileExplore/homePage/HomePage.jsx
+++ src/linkedinProfileExplore/homePage/HomePage.jsx
@@ -11,7 +11,6 @@ export default function Home() {
             <main className="main">
                 <div className="container">
                     <section className="left">
-                        <p className="tag">RESUME WORDED</p>
                         <h1 className="title">
                             Improve your <span className="highlight">resume</span> <br />
                             and <span className="highlight2">LinkedIn profile</span>
@@ -22,8 +21,8 @@ export default function Home() {
                             and job offers.
                         </p>
 
-                        <div className="buttons">
-                            <button className="btn-primary">Get started for free</button>
+                        <div className="home-buttons">
+                            <button className="btn-primary">Get started</button>
                             <button className="btn-secondary">See preview</button>
                         </div>
                     </section>
